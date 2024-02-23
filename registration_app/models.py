03358466cfd49c87af5371d92ebbb4259fedae86
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-23 17:14:23 +0530

    Registration model created

--- registration_app/models.py
+++ registration_app/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
     capacity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Registration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
