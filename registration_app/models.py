6cd8f726957285cbca0f16566ea6c5c8bf9d4e8b
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-23 17:12:35 +0530

    Event model created

--- registration_app/models.py
+++ registration_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
