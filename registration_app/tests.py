39a994424891234459daee08d5112ad4960b3f15
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-25 20:37:11 +0530

    Registered event listing api test case completed

--- registration_app/tests.py
+++ registration_app/tests.py
@@ -5,8 +5,8 @@ from rest_framework.test import APIClient, APIRequestFactory
 from rest_framework_simplejwt.tokens import RefreshToken
 from datetime import timedelta, datetime
 from .models import Event, Registration
-from .views import EventListAPIView, EventRegistrationAPIView
-from .serializers import EventSerializer, EventRegistrationSerializer
+from .views import EventListAPIView, EventRegistrationAPIView, EventRegistrationListAPIView
+from .serializers import EventSerializer, EventRegistrationSerializer, BasicEventRegistrationSerializer
 
 
 class EventListAPITestCase(TestCase):
@@ -143,3 +143,40 @@ class EventRegistrationAPIViewTestCase(TestCase):
                          'Event registration unsuccessful')
         self.assertEqual(response.data['data']
                          ['event'][0], 'Event is at full capacity')
+
+
+class EventRegistrationListAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.event1 = Event.objects.create(
+            name="Test Event 1", capacity=10, valid_until=datetime.now())
+        self.event2 = Event.objects.create(
+            name="Test Event 2", capacity=20, valid_until=datetime.now())
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        self.registration1 = Registration.objects.create(
+            user=self.user, event=self.event1)
+        self.registration2 = Registration.objects.create(
+            user=self.user, event=self.event2)
+
+    def get_jwt_token(self):
+        refresh = RefreshToken.for_user(self.user)
+        return f'Bearer {refresh.access_token}'
+
+    def test_list_registered_events(self):
+        jwt_token = self.get_jwt_token()
+        response = self.client.get(
+            '/api/all-event-registrations/', HTTP_AUTHORIZATION=jwt_token)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['success'], True)
+        self.assertEqual(response.data['message'],
+                         "Event Registration listing successful")
+        self.assertEqual(len(response.data['data']), 2)
+        serialized_events = BasicEventRegistrationSerializer(
+            [self.registration1, self.registration2], many=True).data
+        self.assertEqual(response.data['data'], serialized_events)
+
+    def test_invalid_url(self):
+        response = self.client.get('/invalid-url/')
+        self.assertEqual(response.status_code, 404)
