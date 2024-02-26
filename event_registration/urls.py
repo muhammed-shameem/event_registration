49395a6a6e5d0600787ecce4b61a1d889f5b02a2
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-26 10:45:32 +0530

    documentation url change

--- event_registration/urls.py
+++ event_registration/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     # DRF-Swagger URLs
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
          name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger',
+    path('documentation/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
